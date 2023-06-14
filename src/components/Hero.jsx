a89da8c8eddd9c8bb0c5e62a197a8b5050b4080e
Shubhankargupta691 <shubhankargupta691@gmail.com>
2023-06-15 04:19:01 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,14 +18,12 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`} >
-          <span className='text-[#915EFF]'><a href='https://www.elixircommunity.live/' >ELIXIR</a></span>
+          <span className='text-[#915EFF]'>ELIXIR</span>
           
           </h1>
           <p className={`'text-white mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]`}>
           <h1 className="text-white text-[30px]">Welcome to the Notes section of our website!</h1><br className='sm:block hidden' />
-           This is where you can collect all  your important notes.
-           You can use the notes section to keep track of your work, personal projects, or anything else that's important to you.
-           You can also contribute by providing your stuff.
+           This is where you can collect all your important notes and you can use the notes section to keep track of your work.
           </p>
           <pre />
         </div>
